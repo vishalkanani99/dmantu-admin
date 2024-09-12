2a3cc9b425c4ad5c76975eae3c08f4cad92bf621
Vishal <kvishal0007@gmail.com>
2024-09-13 01:29:27 +0530

    added: refactored tabs components

---
+++ src/components/tab/Tabs.vue
@@ -0,0 +1,93 @@
+<script>
+import { ref, computed, h, defineComponent, shallowRef} from 'vue';
+import TabList from './TabList.vue';
+import TabPanels from './TabPanels.vue';
+import TabPanel from './TabPanel.vue';
+
+export default defineComponent({
+  name: "Tabs",
+  props: {
+    modelValue: {
+      type: Number,
+      default: 1,
+    },
+    vertical: Boolean,
+  },
+  emits: ['update:modelValue'],
+  setup(props, context) {
+    
+    const modelValue = shallowRef(props.modelValue);
+    const activeTab = computed({
+      get: () => modelValue.value,
+      set: (value) => {
+        modelValue.value = value;
+        context.emit('update:modelValue', value);
+      }
+    })
+
+    const slotTabs = computed( () => {
+      let tabs = [];
+      if( context.slots && context.slots.hasOwnProperty('default')) {
+        tabs = context.slots.default();
+      }
+      return tabs.filter((tab) => tab.type 
+          && tab.type.constructor === Object 
+            && tab.type.hasOwnProperty('__name') 
+             && tab.type.__name === 'Tab'
+      );
+    })
+
+    const renderTabs = computed( () => {
+      return slotTabs.value.map((tab, index) => {
+        return h(
+          tab,
+          { vertical: props.vertical, isActive: activeTab.value === (index + 1) ? true : false, onClick: () => changeTab(index + 1) },
+          () => '',  // empty default slot for Tab Component 
+        );
+      });
+    })
+
+    const renderTabsSlots = computed( () => {
+      return slotTabs.value.map((tab, index) => {
+        let tabsSlots = [];
+        if(tab.children && tab.children.constructor === Object && tab.children.hasOwnProperty('default')) {
+          tabsSlots = tab.children.default().map((childEl) => {
+            return h(
+              childEl,
+            );
+          });
+        }
+        return h(
+            TabPanel,
+            {isActive: activeTab.value === (index + 1) ? true : false},
+            () => tabsSlots, // Attached tabs default slot to the TabPanel Component slot
+          );
+      });
+    })
+
+    const changeTab = (tab) => {
+      activeTab.value = tab;
+    }
+
+    // return the render function
+    return () => h(
+      "div",
+      {class: 'grid grid-cols-12 gap-4'},
+      [
+        //Rendring Tabs
+        h(TabList,
+          { vertical: props.vertical }, 
+          () => renderTabs.value
+        ),
+
+        //Rendring Tab Panels
+        h(TabPanels,
+          { vertical: props.vertical },
+          () => renderTabsSlots.value
+        ),
+      ],
+    );
+  },
+
+});
+</script>
--- src/pages/tabs.vue
+++ src/pages/tabs.vue
@@ -8,11 +8,9 @@ import LayoutAuthenticated from '../layouts/LayoutAuthenticated.vue';
 import Section from '../components/section/Section.vue';
 import Card from '../components/card/Card.vue';
 import Tab from '../components/tab/Tab.vue';
-import TabGroup from '../components/tab/TabGroup.vue';
-import TabPanel from '../components/tab/TabPanel.vue';
-import TabPanels from '../components/tab/TabPanels.vue';
-import TabList from '../components/tab/TabList.vue';
+import Tabs from '../components/tab/Tabs.vue';
 
+const activeTab = shallowRef(3);
 </script>
 <template>
   <LayoutAuthenticated>
@@ -23,65 +21,49 @@ import TabList from '../components/tab/TabList.vue';
       btnColor="theme"
     >
       <Card title="Tabs" noFooter>
-        <TabGroup>
-          <TabList>
-            <Tab label="Tab 1"></Tab>
-            <Tab label="Tab 2"></Tab>
-            <Tab label="Tab 3"></Tab>
-            <Tab label="Tab 4"></Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-            </TabPanel>
-            <TabPanel>
-              <ul>
-                <li><span class="w-24 font-bold inline-block">one:</span> One</li>
-                <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
-              </ul>
-            </TabPanel>
-            <TabPanel>
-              <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-            </TabPanel>
-            <TabPanel>
-              <ul>
-                <li><span class="w-24 font-bold inline-block">one:</span> One</li>
-                <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
-              </ul>
-            </TabPanel>
-          </TabPanels>
-        </TabGroup>
+        <Tabs>
+          <Tab label="Tab 1">
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+          </Tab>
+          <Tab label="Tab 2">
+            <ul>
+              <li><span class="w-24 font-bold inline-block">one:</span> One</li>
+              <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
+            </ul>
+          </Tab>
+          <Tab label="Tab 3">
+            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+          </Tab>
+          <Tab label="Tab 4" disabled>
+            <ul>
+              <li><span class="w-24 font-bold inline-block">one:</span> One</li>
+              <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
+            </ul>
+          </Tab>
+        </Tabs>
       </Card>
 
       <Card title="Vertical Tabs" noFooter>
-        <TabGroup vertical>
-          <TabList>
-            <Tab label="Tab 1"></Tab>
-            <Tab label="Tab 2"></Tab>
-            <Tab label="Tab 3"></Tab>
-            <Tab label="Tab 4" disabled></Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-            </TabPanel>
-            <TabPanel>
-              <ul>
-                <li><span class="w-24 font-bold inline-block">one:</span> One</li>
-                <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
-              </ul>
-            </TabPanel>
-            <TabPanel>
-              <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-            </TabPanel>
-            <TabPanel>
-              <ul>
-                <li><span class="w-24 font-bold inline-block">one:</span> One</li>
-                <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
-              </ul>
-            </TabPanel>
-          </TabPanels>
-        </TabGroup>
+        <Tabs v-model="activeTab" vertical>
+          <Tab label="Tab 1">
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+          </Tab>
+          <Tab label="Tab 2">
+            <ul>
+              <li><span class="w-24 font-bold inline-block">one:</span> One</li>
+              <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
+            </ul>
+          </Tab>
+          <Tab label="Tab 3">
+            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+          </Tab>
+          <Tab label="Tab 4" disabled>
+            <ul>
+              <li><span class="w-24 font-bold inline-block">one:</span> One</li>
+              <li><span class="w-24 font-bold inline-block">two:</span> Two</li>
+            </ul>
+          </Tab>
+        </Tabs>
       </Card>
     </Section>
   </LayoutAuthenticated>
