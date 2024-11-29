e7e624078b61efba8bb102fc44c461b54c68f479
Vishal <kvishal0007@gmail.com>
2024-11-30 00:15:16 +0530

    fixed: main section overflow

--- src/components/section/SectionMain.vue
+++ src/components/section/SectionMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-dvh w-dvw lg:w-auto mt-20 p-6 bg-theme-100 transition-[margin] overflow-y-auto">
+  <div class="min-h-dvh w-dvw lg:w-auto mt-20 p-6 bg-theme-100 transition-[margin]">
     <slot></slot>
   </div>
 </template>
