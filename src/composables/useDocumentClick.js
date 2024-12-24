59da27cbf11cc7b3b42bfc5740a269d3b4fe24a9
Vishal <kvishal0007@gmail.com>
2024-12-25 00:12:31 +0530

    fixed: document click

--- src/composables/useDocumentClick.js
+++ src/composables/useDocumentClick.js
@@ -2,11 +2,15 @@ import { ref, watchEffect, toValue, shallowRef, isRef } from 'vue';
 import { useEventListener } from './useEventListener';
 
 export function useDocumentClick(containerRef, outsideTargetCb, onTargetCb = () => {} ){
-  let target, mainTarget;
+  let target, mainTarget, isTargetExist;
 
   function validate(el) {
     target = el.target;
     mainTarget = isRef(containerRef) ? toValue(containerRef) : containerRef;
+    isTargetExist = document.body.contains(el.target);
+
+    if(!isTargetExist || !mainTarget) return;
+
     if(mainTarget.contains(target)){
       onTargetCb(target);
     } else {
